dd14f747edd1947840b370b58853b7596322b10f
Ravi Kumar <ravikdtg@gmail.com>
2023-04-13 16:01:51 +0530

    trying the deployment issues

--- src/pages/admin_dashboard/index.js
+++ src/pages/admin_dashboard/index.js
@@ -1,7 +1,28 @@
 import styles from "../../styles/Admin_dashboard.module.css";
 import Card from "./Card";
-import FunctionItems from "./FunctionItems";
+
+import enquiryImage from "../../../public/images/admin/enquiry.png";
+import postImage from "../../../public/images/admin/post.png";
+import usersImage from "../../../public/images/admin/users.png";
+
 const Index = () => {
+  const FunctionItems = [
+    {
+      id: 1,
+      name: "Incoming Enquiry",
+      image: enquiryImage,
+    },
+    {
+      id: 2,
+      name: "Post notification",
+      image: postImage,
+    },
+    {
+      id: 3,
+      name: "See the Users",
+      image: usersImage,
+    },
+  ];
   return (
     <div className={styles.wrapper}>
       {FunctionItems.map((item) => {
