f76c6fc581b2e043b0fedf726cc663567779314c
Ravi Kumar <ravikdtg@gmail.com>
2023-04-18 00:44:26 +0530

    improving the meta tag

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -29,6 +29,17 @@ export default function App({ Component, pageProps }) {
           name="viewport"
           content="width=device-width, initial-scale=1.0"
         ></meta>
+        <script type="application/ld+json">
+          {`
+          {
+            "@context": "http://schema.org",
+            "@type": "WebSite",
+            "name": "Prayatna Learning Center | Rammurtynagar | Bengaluru",
+            "description": "Best coaching center for the pre-school,high school,class-10,first PUC,second PUC,JEE mains,JEE advance,NATA and K-CET. We give one to one mentorship to all our student and also provide full assistance on the overall development of the student.",
+            "url": "https://prayatnalearning.com"
+          }
+        `}
+        </script>
       </Head>
       <Header />
       <Component {...pageProps} />
