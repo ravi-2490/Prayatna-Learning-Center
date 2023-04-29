f2469c32f94879387a474ffe78d4b3b1a00fe392
Deepshikha <deepshikhasharma34@gmail.com>
2023-04-29 18:41:15 +0530

    added banner and gallery pictures

--- src/components/banner/BannerItems.js
+++ src/components/banner/BannerItems.js
@@ -4,6 +4,11 @@ import ImageThree from "../../../public/images/banner/3.jpg";
 import ImageFour from "../../../public/images/banner/4.jpg";
 import ImageFive from "../../../public/images/banner/5.jpg";
 import ImageSix from "../../../public/images/banner/6.jpg";
+import ImageFone from "../../../public/images/banner/fpage1.jpg";
+import ImageFtwo from "../../../public/images/banner/fpage2.jpg";
+import ImageFthree from "../../../public/images/banner/fpage3.jpg";
+import ImageFFour from "../../../public/images/banner/fpage4.jpg";
+import ImageFFive from "../../../public/images/banner/fpage5.jpg";
 
 const BannerItems = [
   {
@@ -13,26 +18,51 @@ const BannerItems = [
   },
   {
     id: 2,
+    name: "fOne",
+    url: ImageFone,
+  },
+  {
+    id: 3,
     name: "Two",
     url: ImageTwo,
   },
   {
-    id: 3,
+    id: 4,
+    name: "fTwo",
+    url: ImageFtwo,
+  },
+  {
+    id: 5,
     name: "Three",
     url: ImageThree,
   },
   {
-    id: 4,
+    id: 6,
+    name: "fThree",
+    url: ImageFthree,
+  },
+  {
+    id: 7,
     name: "Four",
     url: ImageFour,
   },
   {
-    id: 5,
+    id: 8,
+    name: "fFour",
+    url: ImageFFour,
+  },
+  {
+    id: 9,
     name: "Five",
     url: ImageFive,
   },
   {
-    id: 6,
+    id: 10,
+    name: "fFive",
+    url: ImageFFive,
+  },
+  {
+    id: 11,
     name: "Six",
     url: ImageSix,
   },
--- src/components/gallery/galleryItems.js
+++ src/components/gallery/galleryItems.js
@@ -9,6 +9,11 @@ import imageEight from "../../../public/images/gallery/home/8.jpg";
 import imageNine from "../../../public/images/gallery/home/9.jpg";
 import imageTen from "../../../public/images/gallery/home/10.jpg";
 import imageEleven from "../../../public/images/gallery/home/11.jpg";
+import imageTwelve from "../../../public/images/gallery/home/gImg1.jpg";
+import imageThirteen from "../../../public/images/gallery/home/gImg2.jpg";
+import imageFourteen from "../../../public/images/gallery/home/gImg3.jpg";
+import imageFifteen from "../../../public/images/gallery/home/gImg4.jpg";
+
 const GalleryItems = [
   {
     id: 1,
@@ -53,6 +58,22 @@ const GalleryItems = [
   {
     id: 11,
     url: imageEleven,
+  },
+  {
+    id: 12,
+    url: imageTwelve,
+  },
+  {
+    id: 13,
+    url: imageThirteen,
+  },
+  {
+    id: 14,
+    url: imageFourteen,
+  },
+  {
+    id: 15,
+    url: imageFifteen,
   },
 ];
 
